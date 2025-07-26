156000c3dc718692f395f39e41bdd855d48b0569
damianniebler <87411349+damianniebler@users.noreply.github.com>
2025-07-26 03:52:33 -0500

    Fix electron resource loading

--- electron/main.cjs
+++ electron/main.cjs
@@ -1,32 +1,30 @@
 const { app, BrowserWindow, protocol } = require('electron');
 const path = require('path');
-const url = require('url');
 
 function createWindow() {
-  const win = new BrowserWindow({
-    width: 1000,
-    height: 700,
-    webPreferences: {
-      contextIsolation: true
-    }
-  });
+	const win = new BrowserWindow({
+		width: 1000,
+		height: 700,
+		webPreferences: {
+			contextIsolation: true
+		}
+	});
 
-  const indexPath = url.pathToFileURL(path.join(__dirname, '../build/index.html')).href;
+	// Register a custom protocol to serve files from the build directory. This
+	// avoids "Not allowed to load local resource" errors when loading the app
+	// from the file system.
+	protocol.registerFileProtocol('app', (request, callback) => {
+		const url = request.url.slice('app://'.length);
+		const target = path.join(__dirname, '../build', url);
+		callback({ path: target });
+	});
 
-  protocol.interceptFileProtocol('file', (request, callback) => {
-    const pathname = decodeURIComponent(new URL(request.url).pathname);
-    if (pathname.startsWith('/')) {
-      callback({ path: path.join(__dirname, '../build', pathname.slice(1)) });
-    } else {
-      callback({ path: path.join(__dirname, '..', pathname) });
-    }
-  });
-
-  win.loadURL(indexPath);
+	// Load the built SvelteKit app using the custom protocol
+	win.loadURL('app://index.html');
 }
 
 app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
+	if (process.platform !== 'darwin') app.quit();
 });
