5a05ba174d0545109a4172fcdb43eea09aa29763
damianniebler <damian@theguidingedge.com>
2025-07-26 03:37:24 -0500

    test elec 3

--- svelte.config.js
+++ svelte.config.js
@@ -2,17 +2,19 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: 'index.html'
-    }),
-    paths: {
-      base: '',
-      assets: ''
-    }
-  }
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html'
+		}),
+		// Default to root paths; Electron intercepts requests and serves
+		// files from the build directory so these don't need to be relative
+		paths: {
+			base: '',
+			assets: ''
+		}
+	}
 };
 
 export default config;
